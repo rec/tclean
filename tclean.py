b0cff04671dca84838bf9af8bf78c9d3e97eb32e
Tom Ritchford <tom@swirly.com>
2025-09-15 15:02:27 +0200

    Fix ruff errors

--- tclean.py
+++ tclean.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import queue
 import time
 import shlex
+import subprocess
 
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
 from watchdog.observers import Observer
@@ -13,10 +14,10 @@ RECORDINGS = TO_ROOT / "mirror/LiveTrak L-12"
 ARCHIVE = TO_ROOT / "archive"
 RELEASE = TO_ROOT / "release"
 
-CHANGED_DIRECTORIES = set()  # Directories containing new WAV files
+CHANGED_DIRECTORIES = set[Path]()  # Directories containing new WAV files
 SLEEP_TIME = 0.2
 RUNNING = True
-COMMANDS = queue.Queue()
+COMMANDS = queue.Queue[object]()
 DRY_RUN = False
 
 
@@ -44,7 +45,7 @@ def watch():
 
 
 def backup_directory(source):
-    if source.name == 'Work':
+    if source.name == "Work":
         source = source.parent
     files = source.glob("*.WAV")
     assert files
@@ -67,7 +68,7 @@ def backup_directory(source):
         return
     # Compress to mp3 at 160k
     h, m, s = time[:2], time[2:4], time[4:]
-    release = RELEASE /  f"{year_month}/{year_month}-{day}_{h}-{m}-{s}.mp3"
+    release = RELEASE / f"{year_month}/{year_month}-{day}_{h}-{m}-{s}.mp3"
     _run("ffmpeg", "-i", master[0], "-vn", "-b:a", "160k", release)
 
 
@@ -93,9 +94,10 @@ def on_created():
 
 class WaveFilesChanged(FileSystemEventHandler):
     def on_created(self, e: FileSystemEvent) -> None:
+        assert isinstance(e.src_path, str)
         s = Path(e.src_path)
-        print('WaveFilesChanged', s)
-        if s.suffix == ".WAV" and s.parent.name.startswith('FOLDER'):
+        print("WaveFilesChanged", s)
+        if s.suffix == ".WAV" and s.parent.name.startswith("FOLDER"):
             CHANGED_DIRECTORIES.add(s.parent)
 
 
@@ -112,7 +114,7 @@ def _run(*args, check=True, text=True, **kwargs):
 def _is_card():
     if not CARD.exists():
         return False
-    d = sorted(f.name for f in CARD.iterdir() if not f.name.startswith('.'))
+    d = sorted(f.name for f in CARD.iterdir() if not f.name.startswith("."))
     return d == [f"FOLDER{i:02}" for i in range(1, 11)]
 
 
