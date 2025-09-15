28d8737a267ceca790131c0afb49950a8a491e54
Tom Ritchford <tom@swirly.com>
2025-09-15 20:29:05 +0200

    Modify tclean.py

--- tclean.py
+++ tclean.py
@@ -1,5 +1,7 @@
 from pathlib import Path
+import os
 import queue
+import re
 import time
 import shlex
 import subprocess
@@ -7,12 +9,14 @@ import subprocess
 from watchdog.events import FileSystemEvent, FileSystemEventHandler
 from watchdog.observers import Observer
 
-RSYNC = "rsync --verbose --archive --exclude .*"
-CARD = Path("/Volumes/NO NAME")
-TO_ROOT = Path("/Users/tom/Documents/Work/Music/T.O.™/Recordings/")
-RECORDINGS = TO_ROOT / "mirror/LiveTrak L-12"
-ARCHIVE = TO_ROOT / "archive"
-RELEASE = TO_ROOT / "release"
+RSYNC = "rsync --verbose --archive --exclude=\".*\" --prune-empty-dirs"
+CARD = "/Volumes/NO NAME"
+TO_ROOT = Path("/Users/tom/Documents/Work/Music/T.O.T.M./Recordings/")
+
+# relative to TO_ROOT
+RECORDINGS = "mirror/LiveTrak L-12"
+ARCHIVE = "archive"
+RELEASE = "release"
 
 CHANGED_DIRECTORIES = set[Path]()  # Directories containing new WAV files
 SLEEP_TIME = 0.2
@@ -20,89 +24,110 @@ RUNNING = True
 COMMANDS = queue.Queue[object]()
 DRY_RUN = False
 
+FILE_RE = re.compile(r"\d{6}_\d{6}")
+
+FFMPEG = "ffmpeg", "-y", "-i",
+
 
 def main():
     watch()
 
 
 def watch():
+    os.chdir(str(TO_ROOT))
+    for i in (RELEASE, ARCHIVE, RECORDINGS):
+        Path(i).mkdir(parents=True, exist_ok=True)
+
     o = Observer()
-    o.schedule(CardInserted(), str(CARD))
-    o.schedule(WaveFilesChanged(), str(RECORDINGS), recursive=True)
+    o.schedule(CardInserted(), CARD)
+    o.schedule(WaveFilesChanged(), str(TO_ROOT), recursive=True)
     o.start()
     try:
+        i = 0
+        on_created()
+
         while RUNNING:
+            if not (i := i + 1) % 10:
+                print(".")
             time.sleep(SLEEP_TIME)
             try:
-                task = COMMANDS.get()
+                task = COMMANDS.get(block=False)
             except queue.Empty:
                 pass
             else:
                 task()
+
     finally:
         o.stop()
         o.join()
 
 
 def backup_directory(source):
+    files = sorted(source.glob("*.WAV"))
     if source.name == "Work":
         source = source.parent
-    files = source.glob("*.WAV")
-    assert files
-    assert len(source.name) == 13, source.name
+
+    if not FILE_RE.match(source.name):
+        return
     date, _, time = source.name.partition("_")
+
+    print("Backing up", source)
+
     year, month, day = date[:2], date[2:4], date[4:]
     year_month = f"20{year}-{month}"
-    base = f"{year_month}/{day}/{time}"
+    base = f"{year_month}/{source.name}"
 
-    archive = ARCHIVE / base
-    for i in (RELEASE, archive):
-        i.mkdir(parents=True, exist_ok=True)
+    archive = Path(f"{ARCHIVE}/{base}")
+    archive.mkdir(parents=True, exist_ok=True)
 
     # Copy all .wav as FLAC
-    for f in sorted(files):
-        _run("ffmpeg", "-i", f, archive / f"{f.stem}.flac")
+    for f in files:
+        _run(*FFMPEG, f, archive / f"{f.stem}.flac", capture_output=True)
+
+    if files:
+        ztd = source / "PRJDATA.ZDT"
+        assert ztd.exists()
+        _run("cp", ztd, archive)
 
     if not (master := [f for f in files if f.name == "MASTER.WAV"]):
         print("No master!")
         return
     # Compress to mp3 at 160k
     h, m, s = time[:2], time[2:4], time[4:]
-    release = RELEASE / f"{year_month}/{year_month}-{day}_{h}-{m}-{s}.mp3"
-    _run("ffmpeg", "-i", master[0], "-vn", "-b:a", "160k", release)
+    release = Path(RELEASE) / f"{year_month}/{year_month}-{day}_{h}-{m}-{s}.mp3"
+    release.parent.mkdir(exist_ok=True, parents=True)
+    _run("ffmpeg", "-i", master[0], "-vn", "-b:a", "160k", release, capture_output=True)
 
 
 class CardInserted(FileSystemEventHandler):
     def on_created(self, event: FileSystemEvent) -> None:
-        COMMANDS.put(on_created)
+        COMMANDS.put(lambda: time.sleep(4) or on_created())
 
 
 def on_created():
-    time.sleep(4)
     if not _is_card():
         print("Not a LiveTrak card")
         return
 
     print("Card inserted!")
-    _run(*RSYNC.split(), CARD, RECORDINGS)
+    _run(*RSYNC.split(), CARD, RECORDINGS, capture_output=True, check=False)
 
     dirs = sorted(CHANGED_DIRECTORIES)
     CHANGED_DIRECTORIES.clear()
+
     for d in dirs:
         backup_directory(d)
 
 
 class WaveFilesChanged(FileSystemEventHandler):
-    def on_created(self, e: FileSystemEvent) -> None:
-        assert isinstance(e.src_path, str)
-        s = Path(e.src_path)
-        print("WaveFilesChanged", s)
-        if s.suffix == ".WAV" and s.parent.name.startswith("FOLDER"):
-            CHANGED_DIRECTORIES.add(s.parent)
+    def on_any_event(self, e: FileSystemEvent) -> None:
+        # print("WaveFilesChanged", e.src_path)
+        CHANGED_DIRECTORIES.add(Path(e.src_path).parent)
 
 
 def _run(*args, check=True, text=True, **kwargs):
     args = [str(a) for a in args]
+    print("$", *args)
     if kwargs.get("shell"):
         args = shlex.join(args)
     if not DRY_RUN:
@@ -112,9 +137,10 @@ def _run(*args, check=True, text=True, **kwargs):
 
 
 def _is_card():
-    if not CARD.exists():
+    card = Path(CARD)
+    if not card.exists():
         return False
-    d = sorted(f.name for f in CARD.iterdir() if not f.name.startswith("."))
+    d = sorted(f.name for f in card.iterdir() if not f.name.startswith("."))
     return d == [f"FOLDER{i:02}" for i in range(1, 11)]
 
 
